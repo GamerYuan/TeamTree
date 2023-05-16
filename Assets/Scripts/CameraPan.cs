966549cfdab6ee7039a2b79bfe052204e17972a2
GamerYuan <f2pplayer0801@gmail.com>
2023-05-16 11:18:59 +0800

    CameraMovement
    Basic camera panning

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    private Vector3 startPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            CamPan();
+        }
+    }
+
+    private void CamPan()
+    {
+        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
+        {
+            Vector3 direction = startPos - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = new Vector3();
+            Debug.Log(cam.transform.rotation.eulerAngles.x);
+            if ((cam.transform.rotation * Quaternion.Euler(new Vector3(direction.y * 180, 0, 0))).eulerAngles.x < 80)
+            {
+                cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            }
+            cam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
+            cam.transform.Translate(0, 0, -10);
+
+            startPos = cam.ScreenToViewportPoint(Input.mousePosition);
+
+        }
+    }
+}
