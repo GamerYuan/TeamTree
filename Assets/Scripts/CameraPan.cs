f40b4ee9091adacb21634aa6bc56b7c8caae3a98
GamerYuan <f2pplayer0801@gmail.com>
2023-06-01 20:18:34 +0800

    Pinch to zoom

--- Assets/Scripts/CameraPan.cs
+++ Assets/Scripts/CameraPan.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,48 @@ using UnityEngine;
 public class CameraPan : MonoBehaviour
 {
     [SerializeField] private Camera cam;
+    [SerializeField] private float maxZoom, minZoom, baseZoom, zoomAmp;
     private Vector3 startPos;
+    private float currZoom;
+
+    void Start()
+    {
+        currZoom = baseZoom;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            startPos = cam.ScreenToViewportPoint(Input.mousePosition);
+            Zoom(Input.GetAxis("Mouse ScrollWheel"));
         }
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount == 2)
         {
-            CamPan();
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroDelta = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOneDelta = touchOne.position - touchOne.deltaPosition;
+
+            float deltaMag = (touchZeroDelta - touchOneDelta).magnitude;
+            float currMag = (touchZero.position - touchOne.position).magnitude;
+
+            float diff = currMag - deltaMag;
+
+            Zoom(diff * zoomAmp);
+        } 
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                startPos = cam.ScreenToViewportPoint(Input.mousePosition);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                CamPan();
+            }
         }
     }
 
@@ -33,10 +64,20 @@ public class CameraPan : MonoBehaviour
                 cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
             }
             cam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
-            cam.transform.Translate(0, 0, -10);
+            cam.transform.Translate(0, 0, currZoom);
 
             startPos = cam.ScreenToViewportPoint(Input.mousePosition);
 
+        }
+    }
+
+    private void Zoom(float increment)
+    {
+        if (currZoom + increment < minZoom && currZoom + increment > maxZoom)
+        {
+            cam.transform.Translate(0, 0, increment);
+            currZoom += increment;
+            Debug.Log("CurrZoom = " + currZoom);
         }
     }
 }
