92e71aca8959f975178a38208e1c884e70e39464
GamerYuan <f2pplayer0801@gmail.com>
2023-06-03 16:40:20 +0800

    Material opacity changes

---
+++ Assets/Scripts/TapTheBug/MaterialFadeChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialFadeChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer meshRenderer;
+    private bool fadeDone = false;
+    private Color endColor, startColor;
+    void Start()
+    {
+        meshRenderer = GetComponent<Renderer>();
+        startColor = new Color(1f, 1f, 1f, 0.1f);
+        endColor = new Color(1f, 1f, 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.color = Color.Lerp(startColor, endColor, (10 - transform.parent.position.y)/10);
+    }
+}
