71a70afeee2082c06ed214adb41c602c3b14f734
GamerYuan <f2pplayer0801@gmail.com>
2023-06-13 21:32:58 +0800

    AES-256 encryption functionality

--- Assets/Scripts/DataSerializer.cs
+++ Assets/Scripts/DataSerializer.cs
@@ -2,13 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class DataSerializer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static string fileName = "/SaveData.dat";
+
     private string currentString;
     private float waterVal;
+
+    private static string key = "N7OnL3lf8YasErkERkQAE7+u5R6fspD6QkQZhWhCv/4=";
+    private static string iv = "dt9espR+qOm3M5jlfo5uqQ==";
 
     void Start()
     {
@@ -17,20 +22,23 @@ public class DataSerializer : MonoBehaviour
 
     public void SaveData()
     {
-        string filePath = Application.persistentDataPath + "/SaveData.json";
+        string filePath = Application.persistentDataPath + fileName;
         DataProgress data = new DataProgress();
         data.currentString = currentString;
         data.waterVal = waterVal;
         string jsonString = JsonUtility.ToJson(data);
-        File.WriteAllText(filePath, jsonString);
+        byte[] soup = Encrypt(jsonString);
+        File.WriteAllBytes(filePath, soup);
     }
 
     public void LoadData()
     {
-        string filePath = Application.persistentDataPath + "/SaveData.json";
+        string filePath = Application.persistentDataPath + fileName;
         if (File.Exists(filePath))
         {
-            DataProgress data = JsonUtility.FromJson<DataProgress>(File.ReadAllText(filePath));
+            byte[] soup = File.ReadAllBytes(filePath);
+            string jsonString = Decrypt(soup);
+            DataProgress data = JsonUtility.FromJson<DataProgress>(jsonString);
             currentString = data.currentString;
             waterVal = data.waterVal;
             Debug.Log($"Save File Loaded! {data}");
@@ -47,6 +55,56 @@ public class DataSerializer : MonoBehaviour
     {
         currentString = str;
         waterVal = val;
+    }
+
+    private byte[] Encrypt(string original)
+    {
+        byte[] encrypted;
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(original);
+                    }
+                    encrypted = memoryStream.ToArray();
+                }
+            }
+        }
+        return encrypted;
+    }
+
+    private string Decrypt(byte[] encrypted)
+    {
+        string decrypted;
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream(encrypted))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        decrypted = streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        return decrypted;
     }
 }
 
